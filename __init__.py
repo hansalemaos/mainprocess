656a247c10e108973c4ccf88e203c791695a3fcc
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2023-09-04 19:36:49 -0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,22 +1,43 @@
+import shutil
 import subprocess
 import os
+from shortpath83 import convert_path_in_string
 
-
-def mainprocess(cmd):
+startupinfo = subprocess.STARTUPINFO()
+startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+startupinfo.wShowWindow = subprocess.SW_HIDE
+creationflags = subprocess.CREATE_NO_WINDOW
+invisibledict = {
+    "startupinfo": startupinfo,
+    "creationflags": creationflags,
+    "start_new_session": True,
+}
+def mainprocess(cmd,**kwargs):
+    exists=False
     if isinstance(cmd, str):
         cmd = [cmd]
+    if not os.path.exists(cmd[0]):
+        _exefile = shutil.which(cmd[0])
+        if _exefile:
+            cmd[0] = _exefile
+    else:
+        exists=True
     exefile = cmd[0]
-    exefile = exefile.strip().strip('"').strip()
-    exefile = os.path.normpath(exefile)
-    exefile = f'"{exefile}"'
+    if exists:
+        exefile = exefile.strip().strip('"\' ').strip()
+        exefile = os.path.normpath(exefile)
+        exefile = convert_path_in_string(exefile)
     try:
         arguments = cmd[1:]
     except Exception:
         arguments = []
 
+    cwd = os.path.dirname(exefile)
+    if not cwd or not os.path.exists(str(cwd)):
+        cwd = os.getcwd()
     args_command = " ".join(arguments).strip()
-    wholecommand = f'start "" {exefile} {args_command}'
-    p = subprocess.Popen(wholecommand, shell=True)
+    args_command = convert_path_in_string(args_command)
+    wholecommand = f'start /min "" {exefile} {args_command}'
+    print(f'Executing: {wholecommand}')
+    p = subprocess.Popen(wholecommand, cwd=cwd, env=os.environ.copy(), shell=True,**invisibledict,**kwargs)
     return p
-
-
